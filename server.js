c6954680b02560749da47095745ce3b4addd669d
arha0822 <arunav.harsh@netcracker.com>
2025-06-14 09:31:07 +0530

    added introspection for the server on prod

--- server.js
+++ server.js
@@ -8,22 +8,22 @@ import typeDefs from './graphql/typeDefs.js';
 import resolvers from './graphql/resolvers.js';
 
 const startServer = async () => {
-  const app = express();
-  app.use(cors());
-  const server = new ApolloServer({ typeDefs, resolvers });
+    const app = express();
+    app.use(cors());
+    const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
 
-  await server.start();
-  server.applyMiddleware({ app });
+    await server.start();
+    server.applyMiddleware({ app });
 
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
 
-  app.listen({ port: 4000 }, () =>
-    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
-  );
+    app.listen({ port: 4000 }, () =>
+        console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+    );
 };
 
 startServer();
